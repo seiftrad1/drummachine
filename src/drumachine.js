af1dcb92425d8ffbf07b998415049e8184fc2719
Mohamed TRAD_amai <mohamed.trad@am.ai>
2022-08-23 02:51:03 +0100

    sudden changes

---
+++ src/drumachine.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+
+export default function DrumMachine() {
+  const audioClips = [
+    {
+      keyCode: 81,
+      keyTrigger: "Q",
+      id: "Heater-1",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyCode: 87,
+      keyTrigger: "W",
+      id: "Heater-2",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyCode: 69,
+      keyTrigger: "E",
+      id: "Heater-3",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyCode: 65,
+      keyTrigger: "A",
+      id: "Heater-4",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyCode: 83,
+      keyTrigger: "S",
+      id: "Clap",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyCode: 68,
+      keyTrigger: "D",
+      id: "Open-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyCode: 90,
+      keyTrigger: "Z",
+      id: "Kick-n'-Hat",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyCode: 88,
+      keyTrigger: "X",
+      id: "Kick",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyCode: 67,
+      keyTrigger: "C",
+      id: "Closed-HH",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+  function Pad({ clip }) {
+    const [active, setActive] = useState(false);
+    useEffect(() => {
+      document.addEventListener("keydown", handleKeyPress);
+      return () => {
+        document.removeEventListener("keydown", handleKeyPress);
+      };
+    }, []);
+    const handleKeyPress = (e) => {
+      if (e.keyCode === clip.keyCode) {
+        playSound();
+      }
+    };
+    const playSound = () => {
+      const audioTag = document.getElementById(clip.keyTrigger);
+      setActive(true);
+      setTimeout(() => setActive(false), 200);
+      audioTag.currentTime = 0;
+      audioTag.play();
+    };
+    return (
+      <div
+        onClick={playSound}
+        className={`btn btn-secondary p-4 m-3 ${active && "btn-warning"}`}
+      >
+        <audio src={clip.url} id={clip.keyTrigger} className="clip"></audio>
+        {clip.keyTrigger}
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-info min-vh-100 text-white">
+      <div className="text-center">
+        <h2>Drum machine</h2>
+        {audioClips.map((clip) => (
+          <Pad key={clip.id} clip={clip} />
+        ))}
+      </div>
+    </div>
+  );
+}
